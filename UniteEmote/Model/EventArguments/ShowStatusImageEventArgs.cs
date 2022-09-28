b31a57bd56480e8f80ddd18ccc1abd13e38ca587
vaul.kranak@intel.com <vaul.kranak@intel.com>
2022-09-27 20:23:30 -0700

    removed comments

--- UniteEmote/Model/EventArguments/ShowStatusImageEventArgs.cs
+++ UniteEmote/Model/EventArguments/ShowStatusImageEventArgs.cs
@@ -3,8 +3,6 @@ using UniteEmote.ViewModel.Controls;
 
 namespace UniteEmote.Model.EventArguments
 {
-    ///Create in UnitePlugin.Model.EventArguments
-    ///and add to enum EventArgumentTypes
     [Serializable]
     public class ShowStatusImageEventArgs : HubViewEventArgs
     {
